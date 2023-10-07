c26badedb48dc91afe6c0e2fffab805543bb0851
Michael <mic.plu@wp.pl>
2023-10-07 11:34:03 +0200

    ternary op

---
+++ src/ControlFlow/TernaryOp.java
@@ -0,0 +1,6 @@
+package ControlFlow;
+
+public class TernaryOp {
+
+    int a = (4%2==0?-1:2);
+}
--- src/DataTypes/Transitions/ParsingValues.java
+++ src/DataTypes/Transitions/ParsingValues.java
@@ -4,6 +4,7 @@ public class ParsingValues {
     public static void main(String[] args) {
         String someNumber = "10";
         int no = Integer.parseInt(someNumber);
+        float f = (float) (no / 3);
     }
 
 }
