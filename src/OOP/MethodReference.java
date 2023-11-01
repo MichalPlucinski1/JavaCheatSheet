1e2a388a59c4cec6b95236b6295a46883bd89d30
Pluto <mic.plu@wp.pl>
2023-11-01 23:59:34 +0100

    reference to constructor

--- src/OOP/MethodReference.java
+++ src/OOP/MethodReference.java
@@ -1,6 +1,18 @@
 package OOP;
 
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+
+
+class PlainOld{
+    private static int last_id = 1;
+    private int id;
+
+    public PlainOld(){
+        id = PlainOld.last_id++;
+        System.out.println("Creating a Plain Object " + id);
+    }
+}
 
 public class MethodReference {
 
@@ -8,7 +20,8 @@ public class MethodReference {
     public static void main(String[] args) {
         calculator(Integer::sum, 10, 25);
         calculator(Double::sum, 10.5, 25.3);
-        calculator((a, b) -> a + b, 10, 25);
+        Supplier<PlainOld> reference1 = ()-> new PlainOld();
+        Supplier<PlainOld> reference2 = PlainOld::new;
     }
     private static <T> void calculator(BinaryOperator<T> function, T value1, T value2){
         T result = function.apply(value1, value2);
